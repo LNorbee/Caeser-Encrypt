7b3f7636ae54696f933874f2e5b06489ecea7608
LNorbee <lazar.norbeert@gmail.com>
2022-07-30 04:02:36 +0200

    FIX: shows encrypted written word

--- src/Main.java
+++ src/Main.java
@@ -7,141 +7,146 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Scanner read = new Scanner(System.in);
 		System.out.print("Beírod a kódolni való szöveget? ");
 		String text = read.next();
 		
+		//do {
+		//	System.out.print("Igen 'y' vagy nem 'n' írj: ");
+		//} while (!read.next().equals("y") || !read.next().equals("n"));
+		
 		yesOrNo(text);
 		read.close();
 	}
-	
+
 //---------------------------Metódusok----------------------------------------	
 
-	private static void yesOrNo(String text) throws IOException{
+	private static void yesOrNo(String text) throws IOException {
 		if (text.equals("y")) {
 			System.out.print("Caesar (c) vagy Vigenére (v) titkosítást szeretne használni? ");
 			Scanner scan = new Scanner(System.in);
 			String caesarOrVigenere = scan.next();
-			
+
 			if (caesarOrVigenere.equals("c")) {
-			System.out.print("Add meg a kódolni kívánt szót: ");
+				System.out.print("Add meg a kódolni kívánt szót: ");
 
-			// Megadom a szót String-ként
-			Scanner scan2 = new Scanner(System.in);
-			String word = scan2.next();
+				// Megadom a szót String-ként
+				Scanner scan2 = new Scanner(System.in);
+				String word = scan2.next();
 
-			ceasarEncryption(word);
-			
-			scan.close();		// Ha a "if (cOrV.equals("c")) {" sor elé rakom hibába fut...
-			scan2.close();
-			
+				String encryptWord = ceasarEncryption(word);
+				System.out.println("A kódolt szöveg: " + encryptWord);
+
+				scan.close(); // Ha az "if (caesarOrVigenere.equals("c")) {" sor elé rakom hibába fut...
+				scan2.close();
+
 			} else if (caesarOrVigenere.equals("v")) {
 				System.out.println("Coming soon...");
 			}
-			
+
 		} else if (text.equals("n")) {
 			String fileContent = readFromTxt();
-			
+
 			System.out.println("A kódolt szöveg: " + fileContent + "\n");
-			
+
 			System.out.print("Szeretné menteni egy .txt fájlba a kódolt szöveget? ");
-			
-			Scanner scan4 = new Scanner(System.in);
-			String decide = scan4.next();
-			scan4.close();
-			
-			
+
+			Scanner scan3 = new Scanner(System.in);
+			String decide = scan3.next();
+			scan3.close();
+
 			if (decide.equals("y")) {
 				String result = readFromTxt();
-				
+
 				FileWriter writer = new FileWriter("encrypted_text.txt");
 				writer.write(result);
 				writer.close();
-				
+
 				System.out.println("Sikeresen mentve az 'encrypted_text.txt' fájlba.");
+			} else {
+				System.out.println("Nincs mentve.");
 			}
-			else {
-				System.out.println("End.");
-			}
-			
+
 		} else {
-			System.out.println("Nem megfelelő válasz, add meg újra: ");      // ??? do-while
+			System.out.println("Nem megfelelő válasz, add meg újra: "); // ??? do-while
 		}
 	}
 
- static String readFromTxt() throws FileNotFoundException {
-	String fileContent = "";
-	
-	File file = new File("text.txt");
-	Scanner scan3 = new Scanner(file);
-	while(scan3.hasNextLine()) {		
-		fileContent = fileContent.concat(scan3.nextLine()+ "\n");
+//----------------------------------------------------------------------------
+
+	static String readFromTxt() throws FileNotFoundException {
+		String fileContent = "";
+
+		File file = new File("text.txt");
+		Scanner scan = new Scanner(file);
+		while (scan.hasNextLine()) {
+			fileContent = fileContent.concat(scan.nextLine() + "\n");
+		}
+		scan.close();
+		return ceasarEncryption(fileContent);
 	}
-	scan3.close();
-	return ceasarEncryption(fileContent);
-}
-	
+
 //----------------------------------------------------------------------------
 
 	static String ceasarEncryption(String word) {
 
-		char[] abc = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-				't', 'u', 'v', 'w', 'x', 'y', 'z'};
-		char[] ABC = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-				'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		char[] abc = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+				't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		char[] ABC = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+				'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+		
 
 		// Konvertálom a beírt String-et Char-ba
 		char[] wordStringToChar = word.toCharArray();
 
-		/*for (int i = 0; i < word.length(); i++) {
-			wordStringToChar[i] = word.charAt(i);
-		}*/
+		/*
+		 * for (int i = 0; i < word.length(); i++) { wordStringToChar[i] =
+		 * word.charAt(i); }
+		 */
 
 		// System.out.println(wordChar[1]);
 		// --------------------------------------
 
 		// Összehasonlítom és eltolom a betűket
 		char[] encryptWord = new char[wordStringToChar.length];
-		
+
 		int counter = 0;
 
 		for (int i = 0; i < wordStringToChar.length; i++) {
 			for (int j = 0; j < abc.length; j++) {
-				
+
 				counter++;
-				
+
 				// Ha nagy betűvel kezdődik
 				if (Character.isUpperCase(wordStringToChar[i])) {
 					if (wordStringToChar[i] == ABC[j]) {
 						encryptWord[i] = ABC[j + 3];
-						//System.out.println(counter + 3);
-						
+						// System.out.println(counter + 3);
+
 					}
 				} else if (wordStringToChar[i] == abc[j]) {
 					encryptWord[i] = abc[j + 3];
 				}
 			}
-		}	
+		}
 		// char-t String-é konvertálja
-	
+
 		String encryptString = String.valueOf(encryptWord);
-		
+
 		return encryptString;
 	}
-	
+
 //----------------------------------------------------------------------------
 
-	
 	static int getIndexOfAbcArray(char[] abc, int position) {
-		
+
 		int index = 0;
-		
+
 		for (int i = 0; i < abc.length; i++) {
 			if (abc[position + 3] > abc[abc.length - 1]) {
 				index = 0;
-			}
-			else {
+			} else {
 				index = position + 3;
 			}
 		}
